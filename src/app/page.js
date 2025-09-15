332b6d8e805025a5d7f3a277f706805861d28d07
SatvikSarthak <satvik.sarthak17@gmail.com>
2025-09-15 17:47:09 +0530

    Add WorldMapDemo component and integrate WorldMap; update package dependencies

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import AboutSection from "@/components/About";
 import Banner from "@/components/Banner";
+import { WorldMapDemo } from "@/components/Map";
 export default function HomePage() {
   return (
     <div className=" ">
       <Banner/>
       <AboutSection/>
+      <WorldMapDemo/>
     </div>
   );
 }
