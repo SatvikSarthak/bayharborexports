bbb2818f7ecaecab9cae51c6712542581b9e541d
SatvikSarthak <satvik.sarthak17@gmail.com>
2025-10-14 23:10:03 +0530

    feat: Update icons for global delivery and volume discounts with new images

--- src/app/products/[slug]/page.js
+++ src/app/products/[slug]/page.js
@@ -78,9 +78,7 @@ export default function Page({ params }) {
         },
         { 
           icon: (
-            <svg className="w-10 h-10" fill="currentColor" viewBox="0 0 24 24">
-              <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/>
-            </svg>
+            <Image src="/global.png" alt="Global Delivery" width={30} height={30} />
           ), 
           label: "Global Delivery", 
           gradient: "from-green-50 to-teal-50",
@@ -88,9 +86,19 @@ export default function Page({ params }) {
         },
         { 
           icon: (
-            <svg className="w-10 h-10" fill="currentColor" viewBox="0 0 24 24">
-              <path d="M20 2H4c-1 0-2 .9-2 2v3.01c0 .72.43 1.34 1 1.69V20c0 1.1 1.1 2 2 2h14c.9 0 2-.9 2-2V8.7c.57-.35 1-.97 1-1.69V4c0-1.1-1-2-2-2zm-5 12H9v-2h6v2zm5-7H4V4h16v3z"/>
-            </svg>
+           <svg
+             className="w-10 h-10"
+             fill="none"
+             stroke="#0a4174"
+     strokeWidth="2"
+  viewBox="0 0 24 24"
+>
+  <path
+    d="M20 2H4c-1 0-2 .9-2 2v3.01c0 .72.43 1.34 1 1.69V20c0 1.1 1.1 2 2 2h14c.9 0 2-.9 2-2V8.7c.57-.35 1-.97 1-1.69V4c0-1.1-1-2-2-2zm-5 12H9v-2h6v2zm5-7H4V4h16v3z"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  /></svg>
+
           ), 
           label: "Volume Discounts", 
           gradient: "from-orange-50 to-amber-50",
